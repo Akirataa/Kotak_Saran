35b89f932c2c4d1d48193f6aaf8462f76146d2e1
fathir <fathiracil07@gmail.com>
2024-10-31 14:42:59 +0700

    ubah tampilan dan juga dashboard

--- admin/login.php
+++ admin/login.php
@@ -27,13 +27,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=".css">
     <link rel="stylesheet" href="../resources/style.css">
     <title>Document</title>
 </head>
 <body>
     <div class="login-container">
-    <img src="../resources/superman.png" alt="">
+    <img src="../resources/itech.png" alt="">
         <h2>Login Admin</h2>
 <form action="login.php" method="POST">
     <label for="username">Username:</label>
